82b628ccfe99db6849fc381f019bf1281f88dbc0
Andrew Lin <drewlin@me.com>
2020-09-14 22:19:28 -0400

    add a test api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const testGet = () => {
+    const apiName = 'apiping';
+    const path = '/ping';
+    const myInit = { // OPTIONAL
+      headers: {}, // OPTIONAL
+    };
+  
+    return API
+      .get(apiName, path)
+      .then(response => {
+        // Add your code here
+        console.log('success', response);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  };
+  
+  testGet();
+
   return (
     <div className="App">
       <header className="App-header">
